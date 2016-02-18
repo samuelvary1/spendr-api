53d60ea5ac859067724694ae9be3b48b4d94d527
Sam Vary <sam.vary@gmail.com>
2016-02-17 19:23:23 -0500

    fixed gaps in models, added user id to categories table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, except: [:new, :edit]
   resources :expenses, except: [:new, :edit]
   resources :categories, except: [:new, :edit]
   resources :users
