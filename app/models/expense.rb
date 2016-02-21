69746192cebcb4235f93baebc6b68a1301ec5d02
mathens <athens_maisie@yahoo.com>
2016-02-21 14:15:26 -0500

    fixed saving expense amt and note

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,11 +2,10 @@ class Expense < ActiveRecord::Base
 	belongs_to :category
 	belongs_to :user
 
-def self.create_from_ember(expense_parameters)
-    expense = Expense.where(
-                  amount: expense_parameters[:amount])
-                  .first_or_create
+	def self.create_from_ember(expense_parameters)
+	    expense = Expense.where(
+	                  amount: expense_parameters[:amount],
+										note: expense_parameters[:note]).first_or_create
 
-  end
-
+	  end
 end
