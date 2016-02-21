d913379bd1fd8ae9b868bea84d2bc6ac923e1327
mathens <athens_maisie@yahoo.com>
2016-02-21 16:31:19 -0500

    able to add and save category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,11 @@
-class Category < ActiveRecord::Base	
+class Category < ActiveRecord::Base
 	belongs_to :user
 	has_many :expenses
+
+	def self.create_from_ember(category_parameters)
+	    category = Category.where(
+	                  name: category_parameters[:name])
+	                  .first_or_create
+
+	end
 end
