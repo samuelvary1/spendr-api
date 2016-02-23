f6b7bd5323e305022feef884dce27ad995a54abe
cindyk09 <cindy_kim88@hotmail.com>
2016-02-23 16:26:45 -0500

    trying to get current user working

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-
+devise_for :users, controllers: { sessions: 'sessions' }
 
   namespace :api do
     namespace :v1 do
       resources :users
       resources :expenses, except: [:new, :edit]
       resources :categories, except: [:new, :edit]
-
+      get "users/me", to: "users#me"
+      post "/users/sign_in", to: "sessions#create"
     end
   end
 
-  post "/users/sign_in", to: "sessions#create"
 end
